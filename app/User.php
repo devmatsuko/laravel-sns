8ae0ee1769029d15d115d70c435fb6249d87bfea
devmatsuko <devmatsuko@gmail.com>
2021-02-07 20:14:58 +0900

    [Add]パスワード再設定機能の実装。

--- app/User.php
+++ app/User.php
@@ -2,6 +2,10 @@
 
 namespace App;
 
+// パスワードリセット
+use App\Mail\BareMail;
+use App\Notifications\PasswordResetNotification;
+
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -36,4 +40,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // パスワード再設定通知クラスのインスタンス作成
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new PasswordResetNotification($token, new BareMail()));
+    }
 }
