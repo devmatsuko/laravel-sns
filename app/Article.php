51da841cbbd85958559b094ecc9240c3c27ed82b
devmatsuko <devmatsuko@gmail.com>
2021-02-08 22:16:37 +0900

    [Add]いいね機能の実装。

--- app/Article.php
+++ app/Article.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 // リレーションの追加
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
@@ -12,10 +13,30 @@ class Article extends Model
         'title',
         'body',
     ];
-    
+
     // Userモデルとのリレーション
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
+    }
+
+    // いいねにおけるArticleモデルとUserモデルのリレーション(多対多)
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    // いいねを判定するメソッド
+    public function isLikedBy(?User $user): bool
+    {
+        return $user
+            ? (bool)$this->likes->where('id', $user->id)->count()
+            : false;
+    }
+
+    // いいね数をカウントするメソッド(アクセサ)
+    public function getCountLikesAttribute(): int
+    {
+        return $this->likes->count();
     }
 }
