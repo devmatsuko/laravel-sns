003e8ef58687c8bc35cdaf7a64d4d0b3db3ad79d
devmatsuko <devmatsuko@gmail.com>
2021-02-15 15:45:49 +0900

    [Add]タグ検索機能の実装。

--- app/Tag.php
+++ app/Tag.php
@@ -3,10 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     protected $fillable = [
         'name',
     ];
+
+    // タグ名の前に「#」をつけるアクセサ
+    public function getHashtagAttribute(): string
+    {
+        return '#' . $this->name;
+    }
+
+    // 記事モデルとのリレーション(多対多)
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Article')->withTimestamps();
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ Route::prefix('articles')->name('articles.')->group(function () {
     Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
     Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
 });
+
+// タグ検索のルーティング
+Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
